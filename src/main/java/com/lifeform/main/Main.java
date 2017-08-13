737cbe9cbf8acf35237e0056bfe3967a2e3ea7a8
backspace119 <backspace119rd@gmail.com>
2017-08-13 08:25:57 -0400

    No version change
    +Licensing info. GPL v3

--- src/main/java/com/lifeform/main/Main.java
+++ src/main/java/com/lifeform/main/Main.java
@@ -4,6 +4,26 @@ import com.lifeform.main.data.Options;
 
 /**
  * Created by Bryan on 4/7/2017.
+ * Copyright (C) 2017  Bryan Sharpe
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ *
+ * ANYWHERE THE AFOREMENTIONED LICENSE IS NOT FOUND IN THE SOURCE CODE CREATED BY BRYAN SHARPE
+ * IS STILL COVERED UNDER GPL V3
+ * SEE LICENSE.txt FOR DETAILS.
+ *
  */
 public class Main {
 
