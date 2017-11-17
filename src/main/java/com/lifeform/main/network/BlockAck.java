7999350e40b98801901351d6a0a21a90c77657c0
bryan <backspace119rd@gmail.com>
2017-11-17 06:32:19 -0500

    File was forgotten in last commit, it is added in with this one

---
+++ src/main/java/com/lifeform/main/network/BlockAck.java
@@ -0,0 +1,34 @@
+package com.lifeform.main.network;
+
+import com.lifeform.main.IKi;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+public class BlockAck implements Serializable,Packet {
+
+    public BigInteger height;
+    public boolean verified;
+    @Override
+    public void process(IKi ki, IConnectionManager connMan, PacketGlobal pg) {
+        pg.cancelResend(height);
+        if(verified)
+        {
+            if(ki.getChainMan().currentHeight().compareTo(height) > 0)
+            {
+                pg.sendBlock(height.add(BigInteger.ONE));
+            }else if(ki.getChainMan().currentHeight().compareTo(height) == 0 && !pg.doneDownloading)
+            {
+                pg.doneDownloading = true;
+                connMan.doneDownloading();
+            }
+        }
+    }
+
+
+    //TODO: not putting a type here since we've not made one yet but we're not using these either, possibly deprecate this feature soon
+    @Override
+    public int packetType() {
+        return 0;
+    }
+}
